b8fb45e4bff62d41f77e274dc603181dcfd03716
Elias Lennheimer <lennheimer.elias@gmail.com>
2025-09-23 22:01:01 +0200

    Handeling of command line arguments for zone and date.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,24 +3,63 @@ package com.example;
 import com.example.api.ElpriserAPI;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI api = new ElpriserAPI();
 
-        LocalDate today = LocalDate.now();
-        List<ElpriserAPI.Elpris> todayPrices = api.getPriser(today, ElpriserAPI.Prisklass.SE3);
+        ElpriserAPI.Prisklass zone = parseZone(args);
+        LocalDate date = parseDate(args);
 
 
+
+
+        List<ElpriserAPI.Elpris> todayPrices = api.getPriser(date, zone);
 
         System.out.println(cheapestHour(todayPrices));
         System.out.println(mostExpensiveHour(todayPrices));
         System.out.println(meanPrice(todayPrices));
-
         System.out.println(todayPrices);
 
     }
+
+    private static ElpriserAPI.Prisklass parseZone(String[] args) {
+        ElpriserAPI.Prisklass zone = ElpriserAPI.Prisklass.SE1;
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+        for (int i = 0; i < args.length; i++) {
+            if  (args[i].equals("--zone")) {
+                if (args[i+1].equals("SE1")) {zone = ElpriserAPI.Prisklass.SE1;}
+                if (args[i+1].equals("SE2")) {zone = ElpriserAPI.Prisklass.SE2;}
+                if (args[i+1].equals("SE3")) {zone = ElpriserAPI.Prisklass.SE3;}
+                if (args[i+1].equals("SE4")) {zone = ElpriserAPI.Prisklass.SE4;}
+                }
+            }
+        return zone;
+    }
+
+    private static LocalDate parseDate(String[] args) {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String dateString = "";
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("--date")) {
+                dateString = args[++i];
+                try {
+                    date = LocalDate.parse(dateString, formatter);
+                } catch (DateTimeParseException e) {
+                    throw new IllegalArgumentException("Invalid date format. Use yyyy-MM-dd");
+                }
+            }
+        }
+        return date;
+    }
+
 
     private static double meanPrice(List<ElpriserAPI.Elpris> prices) {
         double sum = 0;
@@ -67,5 +106,22 @@ public class Main {
         }
         return index;
     }
+
+
+//    private static int optimalChargingWindow(int duration, int chargingWindow, List<ElpriserAPI.Elpris> prices) {
+//        double[] window = new double[prices.size()];
+//        for (int i = 0; i < prices.size(); i++) {
+//            window[i] = prices.get(i).sekPerKWh();
+//        }
+//
+//        for (int i = 0; i < prices.size() - chargingWindow; i++) {
+//            int currentSum = 0;
+//            for (int j = 0; j < chargingWindow; j++) {
+//                currentSum = currentSum + window[i+j];
+//            }
+//        }
+//
+//        return -1;
+//    }
 
 }
