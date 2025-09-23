4b56299b137f7be951eb25730bb65785f6bb7f36
Elias Lennheimer <lennheimer.elias@gmail.com>
2025-09-23 19:38:36 +0200

    Reading of Elpriser.api data and finding mean price for the day aswell as cheapest and most expensive hours to charge.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,8 +2,70 @@ package com.example;
 
 import com.example.api.ElpriserAPI;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        ElpriserAPI elpriserAPI = new ElpriserAPI();
+        ElpriserAPI api = new ElpriserAPI();
+
+        LocalDate today = LocalDate.now();
+        List<ElpriserAPI.Elpris> todayPrices = api.getPriser(today, ElpriserAPI.Prisklass.SE3);
+
+
+
+        System.out.println(cheapestHour(todayPrices));
+        System.out.println(mostExpensiveHour(todayPrices));
+        System.out.println(meanPrice(todayPrices));
+
+        System.out.println(todayPrices);
+
     }
+
+    private static double meanPrice(List<ElpriserAPI.Elpris> prices) {
+        double sum = 0;
+        for (ElpriserAPI.Elpris price : prices){
+            sum += price.sekPerKWh();
+        }
+        return sum/prices.size();
+    }
+
+    private static int cheapestHour(List<ElpriserAPI.Elpris> prices) {
+        double largest = prices.getFirst().sekPerKWh();
+        int index = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            ElpriserAPI.Elpris price = prices.get(i);
+
+            if (price.sekPerKWh() == largest) {
+                continue;
+            }
+
+            if (price.sekPerKWh() > largest ) {
+                largest = price.sekPerKWh();
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    private static int mostExpensiveHour(List<ElpriserAPI.Elpris> prices) {
+        double smallest = prices.getFirst().sekPerKWh();
+        int index = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            ElpriserAPI.Elpris price = prices.get(i);
+
+            if  (price.sekPerKWh() == smallest ) {
+                continue;
+            }
+
+            if (smallest > price.sekPerKWh() ) {
+                smallest = price.sekPerKWh();
+                index = i;
+            }
+        }
+        return index;
+    }
+
 }
